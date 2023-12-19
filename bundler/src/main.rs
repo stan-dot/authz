27c2b546b4842b2834044441d0d7ccf2d97b1ec8
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-12-19 11:50:13 +0000

    Export tracing via OTLP gRPC

--- bundler/src/main.rs
+++ bundler/src/main.rs
@@ -23,12 +23,11 @@ use axum::{
 use axum_extra::TypedHeader;
 use clap::Parser;
 use headers::{ETag, HeaderMapExt, IfNoneMatch};
-use opentelemetry::trace::TracerProvider as _;
+use opentelemetry_otlp::WithExportConfig;
 use require_bearer::RequireBearerLayer;
 use serde::Serialize;
 use sqlx::{mysql::MySqlPoolOptions, MySqlPool};
 use std::{
-    any::Any,
     fmt::Debug,
     hash::Hash,
     net::{Ipv4Addr, SocketAddr, SocketAddrV4},
@@ -44,7 +43,7 @@ use tokio::{
 };
 use tower_http::trace::TraceLayer;
 use tracing::instrument;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Layer};
 use url::Url;
 
 /// A wrapper containing a [`Bundle`] and the serialzied gzipped archive
@@ -94,9 +93,9 @@ struct Cli {
     /// The interval at which ISPyB should be polled
     #[arg(long, env = "BUNDLER_POLLING_INTERVAL", default_value_t=humantime::Duration::from(Duration::from_secs(60)))]
     polling_interval: humantime::Duration,
-    /// The URL of the Jaeger instance to send traces to
-    #[arg(long, env = "BUNDLER_JAEGER_URL")]
-    jaeger_url: Option<Url>,
+    /// The URL of the OpenTelemetry collector to send traces to
+    #[arg(long, env = "BUNDLER_TRACING_URL")]
+    tracing_url: Option<Url>,
 }
 
 #[tokio::main]
@@ -104,7 +103,7 @@ async fn main() {
     dotenvy::dotenv().ok();
     let args = Cli::parse();
 
-    let _telemetry = setup_telemetry(args.jaeger_url);
+    setup_telemetry(args.log_level, args.tracing_url).unwrap();
 
     let ispyb_pool = connect_ispyb(args.database_url).await.unwrap();
     let current_bundle = fetch_initial_bundle(&ispyb_pool).await.unwrap();
@@ -125,28 +124,39 @@ async fn main() {
     tasks.join_next().await.unwrap().unwrap()
 }
 
-/// The name given to the service in tracing
-const SERVICE_NAME: &str = "bundler";
-
 /// Sets up Logging & Tracing using jaeger if available
-fn setup_telemetry(jaeger_url: Option<Url>) -> Result<Option<impl Any>, anyhow::Error> {
-    let log_layer = tracing_subscriber::fmt::Layer::default();
-    if let Some(jaeger_url) = jaeger_url {
-        let trace_provider = opentelemetry_jaeger::new_agent_pipeline()
-            .with_service_name(SERVICE_NAME)
-            .with_endpoint(jaeger_url)
-            .build_batch(opentelemetry_sdk::runtime::Tokio)?;
+fn setup_telemetry(
+    log_level: tracing::Level,
+    tracing_url: Option<Url>,
+) -> Result<(), anyhow::Error> {
+    let log_layer = tracing_subscriber::fmt::layer().with_filter(
+        tracing_subscriber::filter::LevelFilter::from_level(log_level),
+    );
+    if let Some(tracing_url) = tracing_url {
+        let tracer = opentelemetry_otlp::new_pipeline()
+            .tracing()
+            .with_exporter(
+                opentelemetry_otlp::new_exporter()
+                    .tonic()
+                    .with_endpoint(tracing_url),
+            )
+            .with_trace_config(opentelemetry_sdk::trace::config().with_resource(
+                opentelemetry_sdk::Resource::new(vec![opentelemetry::KeyValue::new(
+                    "service.name",
+                    "bundler",
+                )]),
+            ))
+            .install_batch(opentelemetry_sdk::runtime::Tokio)?;
         tracing_subscriber::Registry::default()
             .with(log_layer)
-            .with(tracing_opentelemetry::layer().with_tracer(trace_provider.tracer(SERVICE_NAME)))
+            .with(tracing_opentelemetry::layer().with_tracer(tracer))
             .init();
-        Ok(Some(trace_provider))
     } else {
         tracing_subscriber::Registry::default()
             .with(log_layer)
             .init();
-        Ok(None)
     }
+    Ok(())
 }
 
 /// Creates a connection pool to the ISPyB instance at the provided [`Url`]
